8bfcbdfda8a9323bb35638cd7bce2b6ac455c075
Kelvin Mugendi <robert.kevin83@gmail.com>
2023-02-20 13:33:31 +0300

    Change keyboard type in signin

--- lib/screens/auth/sign_in/sign_in.dart
+++ lib/screens/auth/sign_in/sign_in.dart
@@ -253,7 +253,7 @@ class _SignInState extends State<SignIn> {
                                   color: Colors.black87,
                                   fontWeight: FontWeight.w500,
                                   fontSize: 14),
-                              keyboardType: TextInputType.name,
+                              keyboardType: TextInputType.none,
                               decoration: InputDecoration(
                                 suffixIcon: InkWell(
                                   onTap: () {
